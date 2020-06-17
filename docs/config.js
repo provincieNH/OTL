c70b40d04f7c2380185ec8e770f11a86e8cf3c30
Niels Hoffmann <niels.hoffmann@hotmail.com>
2020-06-17 14:53:22 +0200

    nieuwe OTL documentatie

---
+++ docs/config.js
@@ -0,0 +1,73 @@
+var respecConfig = {
+    logos: [{
+        src: "https://noord-holland.mediafiler.net/nh/img/nh.png",
+        href: "http://noord-holland.nl",
+        alt: "Provincie Noord-Holland",
+        width: 341,
+        height: 66,
+        id: 'logo-noord-holland',
+    }],
+    specStatus: "base",
+    xref: true,
+    format: "markdown",
+    editors: [
+        {
+            name: "Niels Hoffmann",
+            company: "Provincie Noord-Holland",
+            companyURL: "https://www.noord-holland.nl/",
+        }
+    ],
+    github: "https://github.com/provincieNH/OTL",
+    shortName: "OTL",
+
+    localBiblio: {
+        ONTOLOGY: {
+              id: "ontology",
+              title: "Ontology (information science)",
+              href: "https://en.wikipedia.org/wiki/Ontology_(information_science)",
+              publisher: "Wikipedia",
+              date: "2019-02-22"
+        },
+        NEN3610: {
+            id: "NEN 3610:2011 nl",
+            title: "NEN 3610:2011 nl - Basismodel geo-informatie - Termen, definities, relaties en algemene regels voor de uitwisseling van informatie over aan de aarde gerelateerde ruimtelijke objecten",
+            href: "https://www.nen.nl/NEN-Shop/Norm/NEN-36102011-nl.htm",
+            status: "Definitief",
+            publisher: "NEN",
+            date: "2011-03-01"
+        },
+        IMGeo: {
+            title: "Informatie Model geografie",
+            href: "https://imgeo.geostandaarden.nl/",
+            status: "Definitief",
+            publisher: "Geonovum",
+            date: "juli 2013"
+        },
+        Assetmanagement: {
+            title: "Wat is assetmanagement",
+            href: "https://www.iampro-portaal.nl/Assetmanagement-kennis/Wat-is-assetmanagement",
+            publisher: "iAMPro",
+            date: "2020"
+        },
+        NTA8035: {
+            id: "NTA 8035:2020 nl",
+            title: "Semantische gegevensmodellering in de gebouwde omgeving ",
+            href: "https://www.nen.nl/NEN-Shop/Norm/NTA-80352020-nl.htm",
+            status: "Definitief",
+            publisher: "NEN",
+            date: "01-04-2020"
+        },
+        TMLO: {
+            title: "Toepassingsprofiel metadatering lokale overheden (TMLO), versie 1.1.",
+            href: "https://www.nationaalarchief.nl/archiveren/kennisbank/tmlo",
+            status: "Definitief",
+            date: "14-4-2014"
+        },
+        IMBOR_LD: {
+            title: "IMBOR (in Linked data-formaat)",
+            href: "https://stichting-crow.github.io/imbor/",
+            date: "16-6-2020"
+        },
+        
+}
+};
